ef18452136830fb80dfa3434dcbc995745b38f88
Muhammad Jazzel Mehmood <jazzelmehmood6@gmail.com>
2021-05-10 03:16:06 +0500

    Fix (2)

--- app/server.py
+++ app/server.py
@@ -7,12 +7,96 @@ import random
 
 import re
 
-
-from utils.encryption import init as initEncryption
-
 UPLOAD_FOLDER = 'uploads'
 KEY_LENGTH = 10
 FILENAME = "test.txt"
+
+def encrypt(filename, key):
+    print(filename, key)
+    return "file"
+
+
+def generateKey():
+    ran = ''.join(random.choices(string.ascii_lowercase + string.digits, k = KEY_LENGTH))    
+    key = str(ran)
+    return key
+
+
+import re
+import string    
+
+
+
+def getWordsFromLineList(sentences):
+    wordsList = []
+    for sentence in sentences:
+        wordsList += re.sub('['+string.punctuation+']', '', sentence).split()
+    return wordsList
+
+def countFrequency(wordsList):
+    wordsWithFrequencies = {}
+    for word in wordsList:
+        wordsWithFrequencies[word] = wordsWithFrequencies.get(word, 0) + 1
+    return list(wordsWithFrequencies.items())
+
+def countFrequencyLength(wordsWithFrequencies):
+    frequencies = {}
+    for word, frequency in wordsWithFrequencies:
+        frequencies[frequency] = frequencies.get(frequency, 0) + 1
+    return list(frequencies.items())
+
+def bubble_sort(lst):
+    if len(lst) == 1:
+        return print(lst)
+        
+    for i in range(len(lst) - 1): 
+        for j in range(0, len(lst) - i -1): 
+            if lst[j][1] < lst[j + 1][1]: 
+                lst[j], lst[j + 1] = lst[j + 1], lst[j] 
+    return lst
+
+def replaceWords(words, maximumFrequencyOccurence, key):
+    replacements = {}
+    for word, frequency in words:
+        if frequency == maximumFrequencyOccurence:
+            wordASCII = ""
+            for letter in word:
+                wordASCII += "{0:03d}".format(ord(letter))
+            newWord = str(ord(key[0])) + key[:len(key) // 2] + str(wordASCII) + key[len(key) // 2:] + str(ord(key[-1]))
+            replacements[word] = newWord
+    return replacements
+    
+def replaceInput(words, replacedWords):
+    for word, replacement in replacedWords.items():
+        if word in words:
+            index = words.index(word)
+            words[index] = replacement
+    return words
+
+def returnEncryptedData(encryptedWords):
+    return " ".join(encryptedWords)
+
+
+def initEncryption(data, key):
+    if type(data) == list:
+        data = "".join(data)
+
+    if not key:
+        key = generateKey()
+    
+    sentences = data.lower().split(".")[:-1]
+
+    words = getWordsFromLineList(sentences)
+    wordsWithFrequencies = countFrequency(words)
+    frequencies = countFrequencyLength(wordsWithFrequencies)
+    maximumFrequencyOccurence = bubble_sort(frequencies)[0][0]
+    replacedWords = replaceWords(wordsWithFrequencies, maximumFrequencyOccurence, key)
+    encryptedWords = replaceInput(words, replacedWords)
+    encryptedData = returnEncryptedData(encryptedWords)
+
+    return encryptedData
+
+
 
 # ? Creating app with Flask
 app = Flask(__name__)
@@ -30,7 +114,6 @@ def index():
             uploaded_file.save(os.path.join(app.root_path, app.config['UPLOAD_FOLDER'], file))
             encrypted = ""
             path = os.path.join(app.root_path, app.config['UPLOAD_FOLDER']) + f"/{FILENAME}"
-            print(path)
             with open(path, "r") as fileReader:
                 encrypted = initEncryption(fileReader.read(), key)
             with open(path, "w") as fileWriter:
@@ -38,8 +121,6 @@ def index():
             return redirect(url_for("result", key=key, filename=FILENAME))  
             # uploaded_file.save(uploaded_file.filename)
         return redirect(url_for('index'))
-
-        
     return render_template('index.htm')
 
 @app.route("/result", methods=["GET"])
@@ -56,13 +137,3 @@ def download(filename):
     return send_file(uploads, as_attachment=True)
     # return send_from_directory(directory=uploads, filename=filename)
 
-
-def encrypt(filename, key):
-    print(filename, key)
-    return "file"
-
-
-def generateKey():
-    ran = ''.join(random.choices(string.ascii_lowercase + string.digits, k = KEY_LENGTH))    
-    key = str(ran)
-    return key
